63af05580ed0469fe14ce2e6f7618fee9ffb9bb0
Naveen Mustang <naveen352kumar@gmail.com>
2019-02-02 21:22:17 +0530

    BottomSheet update

--- app/src/main/java/com/example/mahen/adibha2/about.java
+++ app/src/main/java/com/example/mahen/adibha2/about.java
@@ -34,14 +34,12 @@ public class about extends AppCompatActivity {
         mTitleWindow.setText("Got My Trip");
         StringBuilder stringBuilder = new StringBuilder();
         String someMessage = "Got My Trip is a mobile app for transportation that mingles customers and driver partners in to a mobile\n" +
-                "technology platform. It is a convenient, easy and quick service that fulfills customers requirements in\n" +
+                "technology platform. It is a convenient, easy and quick service that fulfills customers requirements on\n" +
                 "travelling.\n\n" +
                 "GMT ensures to offer AC cabs at affordable fares for customers to travel conveniently in GMT cabs. It is\n" +
                 "easy and fast to book cabs in GMT without any lose of time.\n\n" +
-                "GMT have mingled with driver - partners which givesn an earning opportunity for drivers to grow\n" +
-                "proffesionally and personally\n\n" +
-                "GMT have mingled with driver - partners which givesn an earning opportunity for drivers to grow\n" +
-                "proffesionally and personally.";
+                "GMT have mingled with driver - partners which given an earning opportunity for drivers to grow\n" +
+                "professionally and personally\n";
         for (int i = 0; i < 1; i++) {
             stringBuilder.append(someMessage);
         }
